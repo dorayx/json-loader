6bedb309827ac15ac070be2e6bb3ed146be8655a
dorayx <hongduhui@qq.com>
2015-10-30 01:14:02 +0800

    Strip comments from JSON

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+
+var stripJsonComments = require('strip-json-comments');
+
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
-	var value = typeof source === "string" ? JSON.parse(source) : source;
+	var strippedSource = stripJsonComments(source);
+	var value = (typeof source === "string") ? JSON.parse(strippedSource) : strippedSource;
 	this.value = [value];
 	return "module.exports = " + JSON.stringify(value, undefined, "\t") + ";";
 }
